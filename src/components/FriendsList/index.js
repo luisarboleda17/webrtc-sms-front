3f3ea201e1b7f031384f6eb3b6056d3daa9fd47d
luis_arboleda17 <hello@larboleda.io>
2020-06-29 01:35:54 -0500

    Friends list component implemented

--- src/components/FriendsList/index.js
+++ src/components/FriendsList/index.js
@@ -3,25 +3,36 @@ import './index.scss';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const FriendsList = ({meetingId, children}) => {
+import api from '../../api';
+import Friend from '../Friend';
+
+const FriendsList = ({meetingId}) => {
   const [friends, setfriends] = useState(null);
 
   // Create token for meeting
   useEffect(() => {
     const cancelSource = axios.CancelToken.source();
 
-    // Get meeting data
-    /*api.createMeetingToken(meetingId, cancelSource)
-      .then(response => setMeetingData(response.data.data))
-      .catch(err => console.error(err));*/
+    // Get friends
+    api.getMeetingFriends(meetingId, cancelSource)
+      .then(response => setfriends(response.data.data))
+      .catch(err => console.error(err));
 
     // Unsubscribe
     return () => cancelSource.cancel('View disposed');
   }, [meetingId]);
 
-  return (
-    <div className="friends-list">{children}</div>
-  );
+  if (!friends) {
+    return <div className="friends-list">Loading friends...</div>;
+  } else if (!Array.isArray(friends)) {
+    return <div className="friends-list">{friends}</div>; // Show error
+  } else {
+    return (
+      <div className="friends-list">{
+        friends.map((friend) => <Friend key={friend.phone} name={friend.name} phone={friend.phone} state={friend.state}/>)
+      }</div>
+    );
+  }
 };
 
 export default FriendsList;
