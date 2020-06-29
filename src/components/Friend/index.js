5aa798598815d2f036992d4002d1492a6e687ace
luis_arboleda17 <hello@larboleda.io>
2020-06-29 01:35:40 -0500

    Adde friend state mapping

--- src/components/Friend/index.js
+++ src/components/Friend/index.js
@@ -2,6 +2,21 @@
 import './index.scss';
 import React from 'react';
 
+/**
+ * Map friend state
+ * @param accepted
+ * @returns {string}
+ */
+const getFriendState = accepted => {
+  if (accepted === undefined || accepted === null) {
+    return 'No answer';
+  } else if (accepted) {
+    return 'Accepted';
+  } else {
+    return 'Rejected';
+  }
+};
+
 const Friend = ({name, phone, state}) => (
   <div className="friend row">
     <div className="col-7">
@@ -10,7 +25,7 @@ const Friend = ({name, phone, state}) => (
         <div className="friend__phone col-12">{phone}</div>
       </div>
     </div>
-    <div className="friend__state col-5">{state}</div>
+    <div className="friend__state col-5">{getFriendState(state)}</div>
   </div>
 );
 
